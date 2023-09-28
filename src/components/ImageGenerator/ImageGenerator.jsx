ec312631e9728230dd79752c37456bfeef104724
d3monv3num <namananand891@gmail.com>
2023-09-28 11:46:22 +0530

    Image Generator Additions Commit

--- src/components/ImageGenerator/ImageGenerator.jsx
+++ src/components/ImageGenerator/ImageGenerator.jsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
-
+import './ImageGeneratorStyle.css';
+import DefaultImg from './defaultDreamImg.jpeg';
 const ImageGen = () => {
-    const [imageData, setImageData] = useState(null);
+    const [imageData, setImageData] = useState(DefaultImg);
+    const [imageData1, setImageData1] = useState(DefaultImg);
+    const [imageData2, setImageData2] = useState(DefaultImg);
+
     const HF_API_TOKEN = "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";
+    const HF_API_TOKEN1= "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";
+    const HF_API_TOKEN2="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";
     const data = { inputs: "astronaut on a horse" };
 
     async function query() {
@@ -14,15 +20,38 @@ const ImageGen = () => {
                 body: JSON.stringify(data),
             }
         );
+        const response1 = await fetch(
+            "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0",
+            {
+                headers: { Authorization: `Bearer ${HF_API_TOKEN1}` },
+                method: "POST",
+                body: JSON.stringify(data),
+            }
+        );
+        const response2 = await fetch(
+            "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5",
+            {
+                headers: { Authorization: `Bearer ${HF_API_TOKEN2}` },
+                method: "POST",
+                body: JSON.stringify(data),
+            }
+        );
         const result = await response.blob();
-        console.log(result);
         setImageData(URL.createObjectURL(result));
+        const result1=await response1.blob();
+        setImageData1(URL.createObjectURL(result1));
+        const result2=await response2.blob();
+        setImageData2(URL.createObjectURL(result2));
     }
 
     return (
-        <div>
+        <div className="imageGenContainer">
             <button onClick={query}>ImageGen</button>
+            <div className="imageDisplay">
             <img src={imageData} alt="true" />
+            <img src={imageData1} alt="true" />
+            <img src={imageData2} alt="true" />
+            </div>
         </div>
     );
 }
